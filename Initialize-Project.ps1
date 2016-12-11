3b2981591eec95f58304d3d93a629e724655a07d
sakai.memoru <sakai.memoru@gmail.com>
2016-12-11 22:38:59 +0900

    create

---
+++ Initialize-Project.ps1
@@ -0,0 +1,92 @@
+<#
+.DESCRIPTION
+Create a environment for modules development.
+.SINCE
+2016/12/11
+.author
+sakai.mitsuru
+#>
+
+# commandline parameter
+param (
+  [string]$module_name = 'module_samples'
+)
+
+# set encoding
+[Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
+
+# set module name
+mkdir $module_name
+cd $module_name
+
+
+# setting git repogitry
+git init
+'node_modules' | out-file .gitignore -encoding utf8
+# setting develop folder
+mkdir src
+mkdir lib
+mkdir test
+
+# create index.js
+$index_js = "module.exports = require('./lib');"
+$index_js | out-file index.js -encoding utf8
+
+# create README.md template
+$readme_strings = @"
+$module_name
+======
+
+## Overview
+
+## Description
+
+## Usage
+``````javascript
+var obj = require('.\lib\$module_name');
+``````
+
+## Class Design
+
+module name
+
+## Installation
+``````
+$ npm install
+``````
+
+## Dependencies
+package.json
+``````json
+{
+  "name": "$module_name",
+  "version": "1.0.0",
+  "description": "module description",
+  "main": "index.js",
+  "scripts": {
+    "test": "mocha ./test/*.test.js"
+  },
+  "keywords": [
+    "common"
+  ],
+  "author": "boundary.fixed",
+  "license": "MIT"
+}
+``````
+## Unit Test
+``````
+$ npm test
+``````
+
+## LICENCE
+
+MIT.
+
+"@
+$readme_strings | out-file README.md -encoding utf8
+
+# npm init
+npm init
+
+# kick atom editor
+atom $PWD
---
+++ README.md
@@ -0,0 +1,35 @@
+﻿Initialize-Project.ps1
+======
+## Overview
+Powershell scripts to create a Node.js package's folder with sample template.  
+
+NPM用の package folder を作成して、 sample template を展開する powershell script.
+
+## Description
+* Create folders.
+  + module folder
+    - src  : for coffeescript
+    - lib  : for javascript
+    - test : for test spec
+* Create files.
+  + .gitignore
+  + README.md
+  + index.js  
+* Execute blow
+  + mkdir
+  + git init
+  + npm init
+  + atom $PWD
+
+## Usage
+1. move to workspace directory.
+2. copy this script to workspace directory.
+3. kick this powershell script with a parameter of your package name;
+```powershell
+ps > ./Initialize-Project.ps1 your_package_name
+```
+## Author
+Sakai Mitsuru
+
+## LICENCE
+In project only
