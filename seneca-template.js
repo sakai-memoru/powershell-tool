e3eba94d8f425ea617373f45bf7b324769dca3fa
sakai.memoru <sakai.memoru@gmail.com>
2016-12-12 23:50:44 +0900

    update template source

--- seneca-template.js
+++ seneca-template.js
@@ -1,87 +1,33 @@
 /**
  *
  */
-var Promise = require('bluebird')
 var _ = require('lodash')
 var seneca = require('seneca')()
-var sum = require('./lib/Math-sum')
+// custom module
+var sum = require('./lib/math-sum')
 
 // pattern
-var ptn  = {role: 'Math', cmd: 'sum'}
+var ptn  = {role: 'math', cmd: 'sum'}
 
-// promisify
-var act = Promise.promisify(seneca.act,seneca)
-
-// add function
-seneca.add({cmd: 'resolve'}, function (msg, response_cb) {
-  // FIXME ptnの判定
-  var err = null
-  var result = sum(msg)
-  response_cb(err, {'res': result})
+// register a pattern and a callback function to seneca object
+seneca.add(ptn, function (param, respond_cb) {
+  var err = null;
+  var ret = sum(param);
+  respond_cb(err, { 'retVal' : ret });
 })
-// Return an error to force a rejected promise
-seneca.add({cmd: 'reject'}, function (args, response_cb) {
-  var err = new Error("D'oh! I've been rejected.")
-  response_cb(err,{'res': 'error'});
-});
 
-// Use the new promisified act() with no callback
-act({cmd: 'resolve'})
-  .then(function (result) {
-    // result will be {message: "Yay, I've been resolved!"} since
-    // its guaranteed to resolve
-  })
-  .catch(function (err) {
-    // Catch any error as usual if it was rejected
-  });
+// // listen on microservice
+// seneca.listen()
 
-act({cmd: 'reject'})
-  .then(function (result) {
-    // Never reaches here since we throw an error on purpose
-  })
-  .catch(function (err) {
-    // err will be set with message "D'oh! I've been rejected."
-  });
-
-// // class function
-// var math_sum = function(param){
-//   var cmds = [];
-//   var command = act(param);
-//   cmds.push(command);
-//   Promise.all(cmds)
-//     .then(function(done){
-//       return done.res;
-//     })
-//     .catch(function (err) {
-//       console.error(err);
-//     });
-// }
-
-//module.exports = math_sum;
 // debug
-// var arg  = {left: 10, right: 3}
-// var param = _.merge(ptn,arg);
-//var result = math_sum(param);
+var arg  = {left: 10, right: 12}
+var param = _.merge(ptn,arg);
+math_sum(param);
 
-
-// // promisify
-// function pAct(param){
-//   var promise = new Promise(
-//     function(resolve,reject){
-//       seneca.act(param, function(err,done){
-//         seneca.result = done.res
-//         console.log('log in pAct: ' + seneca.result);
-//         return seneca.result
-//       })
-//     }
-//   );
-//   return promise
-// }
-//
-// var math_sum = function(param){
-//   co(function *(){
-//     var result;
-//     yield result = pAct(param);
-//     yield console.log(result);
-//   })
-// }
+function math_sum(options){
+  // act a seneca's function
+  seneca.act(param,function(err,result){
+    if (err) return console.error(err)
+    console.log(result);
+  })
+}
